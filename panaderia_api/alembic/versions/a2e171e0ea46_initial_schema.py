ce688d63dbb8756fc17d3f57daedec1e9e58cde1
Felipe <lamediatricolor@gmail.com>
2026-02-20 23:54:37 -0500

    sincronizando alembic con la base de datos

---
+++ panaderia_api/alembic/versions/a2e171e0ea46_initial_schema.py
@@ -0,0 +1,21 @@
+"""initial schema
+
+Revision ID: a2e171e0ea46
+Revises:
+Create Date: 2026-02-20
+
+"""
+from alembic import op
+
+revision = 'a2e171e0ea46'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
