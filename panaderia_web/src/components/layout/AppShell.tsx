693f69726c152d32e21c20fba603b47843ab331e
Felipe <lamediatricolor@gmail.com>
2026-03-29 09:41:20 -0500

    ft: implementando app shell con sidebar y header

--- panaderia_web/src/components/layout/AppShell.tsx
+++ panaderia_web/src/components/layout/AppShell.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
-// por implementar
+import { Header } from './Header'
+import { Sidebar } from './Sidebar'
+
 export default function AppShell() {
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
-    <div className="flex h-screen bg-background">
-      <main className="flex-1 overflow-auto p-6">
-        <Outlet />
-      </main>
+    <div className="flex h-screen overflow-hidden bg-background">
+      <Sidebar collapsed={collapsed} onToggle={() => setCollapsed((v) => !v)} />
+
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto">
+          <div className="mx-auto max-w-7xl p-6">
+            <Outlet />
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
